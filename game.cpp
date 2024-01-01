1b9f9802dcdb40bcbf4947a347b710b70aeb398d
GauravDe-git <gm.gauravde@gmail.com>
2024-01-01 17:30:49 +0530

    Assignment: Part -2
    Cpp Fast Track - 3dgep.com
    Part - 2 The Template
    
    Assignment

--- game.cpp
+++ game.cpp
@@ -1,25 +1,70 @@
 #include "game.h"
+
+#include <iterator>
+
 #include "surface.h"
-#include <cstdio> //printf
 
 namespace Tmpl8
 {
+	constexpr Pixel Red = { 0xff0000 };
+	constexpr Pixel Green = { 0x00ff00 };
+	constexpr Pixel Blue = { 0x0000ff };
+	constexpr Pixel Yellow = { 0xffff00 };
+	struct Point
+	{
+		int x, y;
+		Pixel color;
+	};
+
+	// Define a special Point that signifies the end of a letter
+	constexpr Point endOfLetter = { -1, -1, { 0x000000 } };
+
+	constexpr Point points[] = {
+		//Points to plot letter C
+		{ 167, 117, Red },
+		{ 78, 235, Red },
+		{ 167, 358, Red },
+		endOfLetter,
+
+		// Points to plot letter O
+		{ 220, 235, Green },
+		{ 220, 117, Green },
+		{ 300, 117, Green },
+		{ 300, 235, Green },
+		{ 220, 235, Green },
+		endOfLetter,
+
+		// Points to plot letter D
+		{ 350, 117, Blue },
+		{ 350, 358, Blue },
+		{ 400, 235, Blue },
+		{ 400, 117, Blue },
+		{ 350, 117, Blue },
+		endOfLetter,
+
+			// Points to plot letter E
+		{ 450, 117, Yellow },
+		{ 450, 358, Yellow },
+		{ 450, 235, Yellow },
+		{ 500, 235, Yellow },
+		{ 450, 235, Yellow },
+		{ 450, 117, Yellow },
+		endOfLetter,
+	};
+
 	// -----------------------------------------------------------
 	// Initialize the application
 	// -----------------------------------------------------------
 	void Game::Init()
 	{
 	}
-	
+
 	// -----------------------------------------------------------
 	// Close down application
 	// -----------------------------------------------------------
 	void Game::Shutdown()
 	{
 	}
-
-	static Sprite rotatingGun(new Surface("assets/aagun.tga"), 36);
-	static int frame = 0;
 
 	// -----------------------------------------------------------
 	// Main application tick function
@@ -28,13 +73,16 @@ namespace Tmpl8
 	{
 		// clear the graphics window
 		screen->Clear(0);
-		// print something in the graphics window
-		screen->Print("hello world", 2, 2, 0xffffff);
-		// print something to the text window
-		printf("this goes to the console window.\n");
-		// draw a sprite
-		rotatingGun.SetFrame(frame);
-		rotatingGun.Draw(screen, 100, 100);
-		if (++frame == 36) frame = 0;
+
+		for (int i = 1; i < std::size(points); i++)
+		{
+			auto p0 = points[i - 1];
+			auto p1 = points[i];
+
+			// Skip drawing a line if the next point is endOfLetter
+			if (p1.x == endOfLetter.x && p1.y == endOfLetter.y) continue;
+			
+			screen->Line(p0.x, p0.y, p1.x, p1.y, p0.color);
+		}
 	}
 };
